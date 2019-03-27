6d1bb9e1457b612f1e9c626495c8bd9be050c02d
Vivek Singh <vivekkmr45@yahoo.in>
2019-03-27 16:19:37 +0530

    Add New and add function
    Signed-off-by: Vivek Singh <vivekkmr45@yahoo.in>

---
+++ consistenthash/consistenthash.go
@@ -0,0 +1,57 @@
+package consistenthash
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+//Hasher hash function type to hash the key
+type Hasher func([]byte) uint32
+
+//ConsistentHash consistent hash type
+type ConsistentHash struct {
+	//Hash function to hash key and nodes
+	Hash Hasher
+	//Replicas Number of replicas for each node
+	Replicas int
+	//HashRing a sorted array of hashes
+	HashRing []uint32
+	//HashMap a map of hash and it's value
+	HashMap map[uint32]string
+}
+
+//New returns a instance of consistent hash
+func New(replicas int, hasher Hasher) *ConsistentHash {
+	chash := &ConsistentHash{
+		Hash:     hasher,
+		Replicas: replicas,
+		HashMap:  make(map[uint32]string),
+	}
+
+	//Using CRC32 hash becuase It has pretty good peformance compared other
+	//https://softwareengineering.stackexchange.com/questions/49550/which-hashing-algorithm-is-best-for-uniqueness-and-speed
+	if chash.Hash == nil {
+		chash.Hash = crc32.ChecksumIEEE
+	}
+
+	return chash
+}
+
+//Add add keys to consistent hashing
+func (c *ConsistentHash) Add(keys ...string) {
+	for _, key := range keys {
+
+		//Create replicas of nodes to uniform distribution of keys
+		for i := 0; i < c.Replicas; i++ {
+			hash := c.Hash([]byte(key + strconv.Itoa(i)))
+			c.HashRing = append(c.HashRing, hash)
+			c.HashMap[hash] = key
+		}
+	}
+
+	//Sort hash keys on the hashring
+	sort.Slice(c.HashRing, func(i, j int) bool {
+		return c.HashRing[i] < c.HashRing[j]
+	})
+}
